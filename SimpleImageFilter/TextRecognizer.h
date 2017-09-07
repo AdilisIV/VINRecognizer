bffeddea2bbae5ad98ed61a94b21b39c019defa7
“IliaFedorov” <fedoroff.ilia@gmail.com>
2017-09-07 16:00:05 +0500

    commit with tesseract framework. None errors

---
+++ SimpleImageFilter/TextRecognizer.h
@@ -0,0 +1,14 @@
+//
+//  TextRecognizer.h
+//  SimpleImageFilter
+//
+//  Created by user on 9/7/17.
+//  Copyright © 2017 Ski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <TesseractOCR/TesseractOCR.h>
+
+@interface TextRecognizer : NSObject
+
+@end
---
+++ SimpleImageFilter/TextRecognizer.m
@@ -0,0 +1,13 @@
+//
+//  TextRecognizer.m
+//  SimpleImageFilter
+//
+//  Created by user on 9/7/17.
+//  Copyright © 2017 Ski. All rights reserved.
+//
+
+#import "TextRecognizer.h"
+
+@implementation TextRecognizer
+
+@end
